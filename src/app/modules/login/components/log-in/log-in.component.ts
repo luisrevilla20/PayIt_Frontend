376bc953a8fa9e26621b8db49cd6f1e5b460d57a
simon <simon.mettag@gmail.com>
2020-06-05 19:27:57 -0500

    added signin routing and modified css for mobile view

--- src/app/modules/login/components/log-in/log-in.component.ts
+++ src/app/modules/login/components/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToPage(pageName: string):void {
+    this.router.navigate([pageName]);
   }
 
 }
